2871fc573f80807120545320aa583cd7b8b58fb4
hyuk12 <hotman7025@naver.com>
2022-11-17 17:20:00 +0900

    빈에 등록하는 초기화와 종료

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,8 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+
+public class NetworkClient  {
 
     private String url;
 
@@ -27,15 +26,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init() {
         System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+
+    public void close() {
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
